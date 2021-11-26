87ffd28afc20ca59ae3b4a45de44ae2e25385aad
Dmitry <dmsobolev@yahoo.com>
2021-11-26 15:19:51 +0300

    Separating Modules

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,6 @@
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
+mod number_guessing_game;
+use number_guessing_game::guessing_game;
 
 fn main() {
-    println!("Guess the number!");
-
-    let secret_number = rand::thread_rng().gen_range(1..=0b1100100);
-    loop {
-        println!("Please imput your guess:");
-
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Could you input number instead of a characters?");
-                continue;
-            }
-        };
-
-        println!("You guessed: {}", guess);
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
-    }
+    guessing_game()
 }
