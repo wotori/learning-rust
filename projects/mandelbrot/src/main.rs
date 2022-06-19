4b5f66176597945cc357faea16685ce3baedf90b
Wotori <10486621+wotori@users.noreply.github.com>
2022-06-19 19:46:50 +0300

    Mandelbrot set core function

--- projects/mandelbrot/src/main.rs
+++ projects/mandelbrot/src/main.rs
@@ -1,5 +1,8 @@
 extern crate num;
-
+/*
+Множество Мандельброта определяется как множество комплексных чисел с, для которых z не устремляется в бесконечность.
+The Mandelbrot set is defined as the set of complex numbers c for which z does not go to infinity.
+ */
 use num::Complex;
 
 #[allow(dead_code)]
@@ -8,6 +11,17 @@ fn complex_square_add_loop(c: Complex<f64>) {
     loop {
         z = z * z + c
     }
+}
+
+fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    for i in 0..limit {
+        z = z * z + c;
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+    }
+    None
 }
 
 fn main() {
